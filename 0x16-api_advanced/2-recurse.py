b6d9700acba55a210542edfabc72d1a41b5cf71d
Law93D <lawtaden@gmail.com>
2024-05-07 16:33:17 +0200

    returns a list

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+2-recurse
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Recursively returns a list containing the titles of all hot articles for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"limit": 100, "after": after}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        if posts:
+            for post in posts:
+                hot_list.append(post['data']['title'])
+            after = data['data']['after']
+            if after:
+                return recurse(subreddit, hot_list, after)
+            else:
+                return hot_list
+        else:
+            return None
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        result = recurse(sys.argv[1])
+        if result is not None:
+            print(len(result))
+        else:
+            print("None")
