20a94d849c4e514fe0621b8ce266bfef2cb45125
Law93D <lawtaden@gmail.com>
2024-05-07 16:26:51 +0200

    queries and returns

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print(number_of_subscribers(sys.argv[1]))
+        print(number_of_subscribers(sys.argv[1])))
