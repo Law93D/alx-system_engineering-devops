b15f3eb363b1099df15799f95efc02e4b4ac2e69
Law93D <lawtaden@gmail.com>
2024-05-10 17:58:43 +0200

    apache error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,31 +1,6 @@
 # Puppet manifest to fix Apache returning a 500 error
 
-# Define an exec resource to restart Apache service
-exec { 'restart_apache':
-  command     => '/usr/sbin/service apache2 restart',
-  path        => '/usr/sbin:/usr/bin:/sbin:/bin',
-  refreshonly => true,
-}
-
-# Fix the issue causing 500 Internal Server Error
-file { '/var/www/html/wp-content':
-  ensure  => 'directory',
-  owner   => 'www-data',
-  group   => 'www-data',
-  recurse => true,
-  mode    => '0755',
-}
-
-file { '/var/www/html/wp-content/uploads':
-  ensure  => 'directory',
-  owner   => 'www-data',
-  group   => 'www-data',
-  recurse => true,
-  mode    => '0755',
-}
-
-# Notify the exec resource to restart Apache when the fix is applied
-notify { 'Restart Apache after fixing the issue':
-  require => [File['/var/www/html/wp-content'], File['/var/www/html/wp-content/uploads']],
-  subscribe => Exec['restart_apache'],
+exec { 'fix wordpress':
+  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path        => '/usr/local/bin:/bin',
 }
